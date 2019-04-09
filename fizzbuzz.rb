156e48bc539f190f3f9fdecf6322e7e7368d5589
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-09 17:45:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 
-    "Fizz"
+    return "Fizz"
   end
   if int % 5 == 0 
-    "Buzz"
+    return "Buzz"
   end
   if int % 15 == 0 
-    "FizzBuzz"
+   return "FizzBuzz"
   end
 end
